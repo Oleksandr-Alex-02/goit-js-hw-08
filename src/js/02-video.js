ce6c56f67935d9e7af5211c8e540a12aa63bd19c
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-03 15:38:36 +0300

    task-2-dz-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from '../../node_modules/lodash/throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(assaaes, 1000));
+
+function assaaes({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
