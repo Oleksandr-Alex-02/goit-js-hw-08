1dc3e9b046baf6db87319318e2003f7876a709a4
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-04 19:59:33 +0300

    +-

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,4 +2,21 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', console.log(value));
+form.addEventListener('input', valueForm);
+
+const formData = {};
+const pars = JSON.stringify(formData);
+
+const ss = localStorage.setItem('dgdg', pars);
+const ssss = localStorage.getItem('dgdg');
+const ggg = JSON.parse(ssss);
+console.log(ggg);
+
+// localStorage.setItem(dataForm);
+
+function valueForm(e) {
+  formData[e.target.name] = e.target.value;
+}
+
+console.log(formData);
+console.log();
