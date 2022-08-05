d34a8b7c903bb95aa2e55e0d65e3d4f8c337d249
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-08-05 16:01:35 +0300

    task-3
    конструктор для сховища

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,48 @@
-import throttle from 'lodash.throttle';
+import { save, parse, removeKey } from './storage.js';
+// import throttle from 'lodash.throttle';
+
+const formData = {};
+console.log(formData);
 
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', valueForm);
-
-const formData = {};
-const pars = JSON.stringify(formData);
-
-const ss = localStorage.setItem('dgdg', pars);
-const ssss = localStorage.getItem('dgdg');
-const ggg = JSON.parse(ssss);
-console.log(ggg);
-
-// localStorage.setItem(dataForm);
-
-function valueForm(e) {
+form.addEventListener('input', function (e) {
   formData[e.target.name] = e.target.value;
+  save('feedback-form-state', JSON.stringify(formData));
+  localStorageValue();
+});
+console.log(parse('feedback-form-state'));
+// localStorage
+
+function localStorageValue() {
+  try {
+    return parse('feedback-form-state') === null
+      ? undefined
+      : console.log('10');
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+
+  console.log(parse('feedback-form-state'));
 }
 
-console.log(formData);
-console.log();
+// SABMIT;
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return;
+    alert('Please fill in all the fields!');
+  }
+  removeKey('feedback-form-state');
+  const formData = new FormData(form);
+  const valuesFotm = Object.fromEntries(formData.entries());
+  console.log(valuesFotm);
+  e.currentTarget.reset();
+}
---
+++ src/js/storage.js
@@ -0,0 +1,25 @@
+export const save = (key, value) => {
+  try {
+    localStorage.setItem(key, value);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+export const parse = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export const removeKey = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : localStorage.removeItem(key);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
